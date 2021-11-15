9166f2bdcdfbd8e45ee791ed6467f5ec9ecaf691
BarnaGergely <barna.gergely.hu@gmail.com>
2021-11-15 16:09:12 +0100

    start

--- main.c
+++ main.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
 
+int betoltoMeret(char forras[100], int *meret){
+    FILE *infile;
+
+    if (!(infile = fopen(forras, "r"))) {
+        return 1;
+    }
+    fscanf("%d %d", meret[0], meret[1]);
+
+    fclose(infile);
+    return 0;
+}
+
+int betoltoMeteor(char forras[100], int *meteorok){
+    FILE *infile;
+
+    if (!(infile = fopen(forras, "r"))) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int kiiro(int szam, char file[100]){
+    FILE *outfile;
+
+    if (!(outfile = fopen("ki.txt", "w"))) {
+        fclose(outfile);
+        return 1;
+    }
+    fprintf(outfile, "%.3lf\n", szam);
+    fclose(outfile);
+
+    return 0;
+}
+
 int main() {
-    printf("Hello, World!\n");
+
+    /* BETOLTES */
+    int meret[2];
+    if (betoltoMeret("be.txt", &meret) == 1){
+
+        return 1;
+    }
+
+
+    /* SZAMOLAS */
+
+
+
     return 0;
 }
