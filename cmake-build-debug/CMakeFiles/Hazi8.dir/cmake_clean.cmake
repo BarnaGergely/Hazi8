e15b7201c2db00384f81c02fe2b0c8d72540e341
BarnaGergely <barna.gergely.hu@gmail.com>
2021-11-15 14:08:17 +0100

    start

---
+++ cmake-build-debug/CMakeFiles/Hazi8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hazi8.dir/main.c.o"
+  "Hazi8"
+  "Hazi8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Hazi8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
